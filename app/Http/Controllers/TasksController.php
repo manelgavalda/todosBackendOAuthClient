4d124b58fdb4d20167f4b92cd9801466bdf3aa9c
manelgavalda <manelgavalda@iesebre.com>
2017-01-11 12:35:37 +0100

    TasksController to get token

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\AccessToken;
 use Illuminate\Http\Request;
 
+/**
+ * Class TasksController
+ * @package App\Http\Controllers
+ */
 class TasksController extends Controller
 {
-    //
     /**
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
-        $data = [];
+        $access_token = null;
+        try {
+
+            $access_token = AccessToken::where('user_id',1)->token;
+        } catch (\Exception $e) {
+
+            redirect('/redirect');
+        }
+        $data = [
+            "access_token" => $access_token
+        ];
         return view('tasks',$data);
     }
 
